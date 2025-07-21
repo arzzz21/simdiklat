519149ecfd1d7ce7f2ee177e03477af397aa53fb
Arizal <muhammadarizal33@gmail.com>
2025-07-21 16:48:09 +0700

    Fix tampilan pegawai

--- resources/views/manajer/laporan/index.blade.php
+++ resources/views/manajer/laporan/index.blade.php
@@ -13,8 +13,9 @@
                     <th>#</th>
                     <th>Nama Pegawai</th>
                     <th>Pelatihan</th>
+                    <th>Berkas Laporan</th>
                     <th>Status</th>
-                    <th>Action</th>
+                    <th width="10%">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -22,14 +23,28 @@
                 <tr>
                     <td>{{ $loop->iteration }}</td>
                     <td>{{ $laporan->pegawai->nama }}</td>
-                    <td>{{ $laporan->pelatihan->nama }}</td>
+                    <td>
+                        {{ $laporan->pelatihan->nama }}<br>
+                        <small><strong>Tempat : </strong>{{ $laporan->pelatihan->tempat }}</small><br>
+                        <small><strong>Waktu : </strong>{{ $laporan->pelatihan->tanggal_mulai }} s/d {{ $laporan->pelatihan->tanggal_selesai }}</small><br>
+                    </td>
+                    <td>
+                        @if ($laporan)
+                            <a href="{{ asset('storage/'.$laporan->file_laporan) }}"target="_blank" class="btn btn-sm btn-info">File Laporan</a><br>
+                            <a href="{{ asset('storage/'.$laporan->file_surat_tugas) }}"target="_blank" class="btn btn-sm btn-warning">File Surat Tugas</a>
+                        @endif
+                    </td>
                     <td>
                         <span class="badge bg-{{ $laporan->status == 'disetujui' ? 'success' : ($laporan->status == 'revisi' ? 'danger' : 'secondary') }}">
                             {{ ucfirst($laporan->status) }}
                         </span>
                     </td>
                     <td>
-                        <a href="{{ route('manajer.laporan.show', $laporan->id) }}" class="btn btn-sm btn-primary">Lihat</a>
+                        @if ($laporan->status === "disetujui")
+                            <span>Sudah Diverifikasi</span>
+                        @else
+                            <a href="{{ route('manajer.laporan.show', $laporan->id) }}" class="btn btn-sm btn-primary">Lihat</a>
+                        @endif
                     </td>
                 </tr>
                 @endforeach
